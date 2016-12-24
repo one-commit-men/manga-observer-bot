43138cc2b8df24c723b3592e3629a5cebab9d7c0
KsushaPetrenko <amfitrita62@rambler.r>
2016-12-24 17:55:50 +0200

    Added support of ReadManga

--- src/main/kotlin/ua/ddovgal/trackerKunBot/source/SourceManager.kt
+++ src/main/kotlin/ua/ddovgal/trackerKunBot/source/SourceManager.kt
@@ -5,9 +5,13 @@ import ua.ddovgal.trackerKunBot.entity.Title
 
 object SourceManager {
 
-    private val sources = emptyList<Source>()
+    private val sources = listOf(
+            ReadMangaSource(),
+            MintMangaSource()
+    )
 
-    fun searchForTitle(name: String): List<Title> = emptyList()
+    fun searchForTitle(name: String): List<Title> =
+            sources.map { it.searchForTitle(name) }.reduce { list1, list2 -> list1.plus(list2) }
 
     fun getSourceByTitleUrl(url: String): Source = sources.first { url.contains(it.url) }
 }
